41e0fb4bd1619e093d8ac64659fae2f4777e2f35
Abdulazeez <azeezmuritalaadeniyi@gmail.com>
2025-04-14 22:28:10 +0100

    added insert,search and tranvalse functionalities

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -1,4 +1,54 @@
 package bst;
 
 public class BinarySearchTree {
+    Node root;
+    public BinarySearchTree() {
+        root = null;
+    }
+    void insertValue(int data) {
+        root = insertRec(root, data);
+    }
+
+    Node insertRec(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        }
+        else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+        return root;
+    }
+
+    boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    boolean searchRec(Node root, int key) {
+        if (root == null)
+            return false;
+
+        if (key == root.data)
+            return true;
+
+        return key < root.data
+                ? searchRec(root.left, key)
+                : searchRec(root.right, key);
+    }
+
+    void inorder() {
+        inorderRec(root);
+        System.out.println();
+    }
+
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    }
 }
